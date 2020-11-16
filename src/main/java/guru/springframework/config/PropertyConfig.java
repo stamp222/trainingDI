4c04325a59a778fa519114fc89e1441a1c7ec8c2
JSzyper <jacek.szyper@savangard.com>
2020-11-16 16:10:47 +0100

    Environment variable

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,15 +2,20 @@ package guru.springframework.config;
 
 import guru.springframework.exmplaeBeans.FakeDataSource;
 import guru.springframework.exmplaeBeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${phlodx.username}")
     String user;
@@ -33,7 +38,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
